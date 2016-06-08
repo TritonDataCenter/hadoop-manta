1abaf2966557d3c896a61db954d8a9cb195e14dc
Elijah Zupancic <elijahz@snapskout.com>
2016-06-07 17:35:58 -0700

    Added explicit key path to unit test.

--- src/test/java/com/joyent/hadoop/fs/manta/MantaConfigTest.java
+++ src/test/java/com/joyent/hadoop/fs/manta/MantaConfigTest.java
@@ -1,10 +1,11 @@
 package com.joyent.hadoop.fs.manta;
 
-import com.joyent.hadoop.fs.manta.MantaFileSystem;
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
@@ -17,6 +18,8 @@ public class MantaConfigTest {
 
         config.set("manta.user", "testuser");
         config.set("manta.key_id", "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00");
+        config.set("manta.key_path", FileUtils.getUserDirectory().toString()
+                + File.separator + ".ssh/id_rsa");
 
         URI uri = URI.create("manta:///");
         FileSystem fs = FileSystem.get(uri, config);
